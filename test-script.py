e0d983d2867f44a2d5c885317d748a0427eb50f6
William Théroux <info@williamtheroux.com>
2020-07-10 02:05:59 -0400

    Individual commands

--- test-script.py
+++ test-script.py
@@ -3,5 +3,7 @@ from smtplib import SMTP
 with SMTP(port=25) as smtp:
 	print(smtp.connect())
 	print(smtp.helo())
-	print(smtp.sendmail("Mail Sender <mail@mail.mail>", ["Recipient <rcpt@rcpt.rcpt>"], "Message"))
+	print(smtp.mail("mail@mail.mail"))
+	print(smtp.rcpt("rcpt@rcpt.rcpt"))
+	print(smtp.data("This is my message"))
 	print(smtp.quit())
