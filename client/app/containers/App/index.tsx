976d72e7cfc2e21ccce3afe4d599330a69b51752
William Théroux <info@williamtheroux.com>
2020-08-17 22:23:44 -0400

    started work on routing

--- client/app/containers/App/index.tsx
+++ client/app/containers/App/index.tsx
@@ -1,5 +1,25 @@
 import React from 'https://jspm.dev/react@16.13.1';
+import {
+	BrowserRouter as Router,
+	Switch,
+	Route,
+	Link
+} from "https://jspm.dev/react-router-dom@5.2.0";
 
 export const App = () => (
-	<h1>Posture</h1>
+	<Router>
+		<h1>Posture</h1>
+		<div>We have routes</div>
+		<Switch>
+			<Route path="/settings">
+				<div>Settings</div>
+			</Route>
+			<Route exact path={["/", "/message/:id"]}>
+				<div>Messages</div>
+			</Route>
+			<Route path="*">
+				<div>Error 404</div>
+			</Route>
+		</Switch>
+	</Router>
 );
--- client/app/containers/Root/index.tsx
+++ client/app/containers/Root/index.tsx
@@ -1,11 +1,5 @@
 import React from 'https://jspm.dev/react@16.13.1';
 import {
-	BrowserRouter as ReactRouter,
-	Switch,
-	Route,
-	Link
-} from "https://jspm.dev/react-router-dom@5.2.0";
-import {
 	RecoilRoot
 } from 'https://jspm.dev/recoil@0.0.8';
 import { App } from '../App/index.tsx';
